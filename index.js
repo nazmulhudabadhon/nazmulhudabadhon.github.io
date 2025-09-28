74a153cc5f8626dd407c594a64d69230c9c50b1b
Nazmul Huda Badhon <badhon15-4772@diu.edu.bd>
2025-09-28 18:00:34 +0600

    Portfolio

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sentences = [
-        "Looking for Research Assistantship Position",
         "A Researcher in Deep Learning, Medical Imaging",
+        "Looking for Research Assistantship Position",
     ];
 
     const textContainer = document.getElementById("animatedText");
